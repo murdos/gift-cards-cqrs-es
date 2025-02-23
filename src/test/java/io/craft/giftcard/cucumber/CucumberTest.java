d8e1712e7e2dfecdb31da965986adf4e71ffb12a
Aurélien Mino <aurelien.mino@gmail.com>
2025-02-23 16:07:05 +0100

    Apply module: spring-boot-cucumber-mvc

---
+++ src/test/java/io/craft/giftcard/cucumber/CucumberTest.java
@@ -0,0 +1,20 @@
+package io.craft.giftcard.cucumber;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+import io.craft.giftcard.ComponentTest;
+
+@Suite(failIfNoTests = false)
+@ComponentTest
+@IncludeEngines("cucumber")
+@SuppressWarnings("java:S2187")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.craft.giftcard")
+@ConfigurationParameter(
+  key = PLUGIN_PROPERTY_NAME,
+  value = "pretty, json:target/cucumber/cucumber.json, html:target/cucumber/cucumber.htm, junit:target/cucumber/TEST-cucumber.xml"
+)
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/features")
+public class CucumberTest {}
