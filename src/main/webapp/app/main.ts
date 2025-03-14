c26ea0f5751ef190c4fdf56aa06352ab69bf507c
cedruck <cedruck@protonmail.com>
2025-03-14 12:14:28 +0100

    feat: enable front / back communication

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,7 +1,11 @@
+import axios from 'axios';
 import { createApp } from 'vue';
 import AppVue from './AppVue.vue';
 import router from './router';
 // jhipster-needle-main-ts-import
+
+// Configure Axios base URL
+axios.defaults.baseURL = 'http://localhost:8080';
 
 const app = createApp(AppVue);
 app.use(router);
