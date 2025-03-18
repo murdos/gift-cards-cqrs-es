9948b33868e83dd3600072dcf279bda4ed00e3c0
Aurélien Mino <aurelien.mino@gmail.com>
2025-03-18 22:09:45 +0100

    fix: add missing @Validated annotation on rest controller

--- src/main/java/io/craft/giftcard/giftcard/infrastructure/primary/GiftCardCommandController.java
+++ src/main/java/io/craft/giftcard/giftcard/infrastructure/primary/GiftCardCommandController.java
@@ -13,9 +13,15 @@ import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/api/gift-cards")
 class GiftCardCommandController {
 
